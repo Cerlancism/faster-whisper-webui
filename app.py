93c4867090b63d37dd9ca84a4cdd9681c1c90fb1
Kristian Stangeland <kr_stang@hotmail.com>
2022-09-23 12:27:44 +0200

    Cleanup code

--- app.py
+++ app.py
@@ -12,111 +12,29 @@ import ffmpeg
 # Limitations (set to -1 to disable)
 INPUT_AUDIO_MAX_DURATION = 120 # seconds
 
-LANGUAGES = [
-    "English",
-    "Chinese",
-    "German",
-    "Spanish",
-    "Russian",
-    "Korean",
-    "French",
-    "Japanese",
-    "Portuguese",
-    "Turkish",
-    "Polish",
-    "Catalan",
-    "Dutch",
-    "Arabic",
-    "Swedish",
-    "Italian",
-    "Indonesian",
-    "Hindi",
-    "Finnish",
-    "Vietnamese",
-    "Hebrew",
-    "Ukrainian",
-    "Greek",
-    "Malay",
-    "Czech",
-    "Romanian",
-    "Danish",
-    "Hungarian",
-    "Tamil",
-    "Norwegian",
-    "Thai",
-    "Urdu",
-    "Croatian",
-    "Bulgarian",
-    "Lithuanian",
-    "Latin",
-    "Maori",
-    "Malayalam",
-    "Welsh",
-    "Slovak",
-    "Telugu",
-    "Persian",
-    "Latvian",
-    "Bengali",
-    "Serbian",
-    "Azerbaijani",
-    "Slovenian",
-    "Kannada",
-    "Estonian",
-    "Macedonian",
-    "Breton",
-    "Basque",
-    "Icelandic",
-    "Armenian",
-    "Nepali",
-    "Mongolian",
-    "Bosnian",
-    "Kazakh",
-    "Albanian",
-    "Swahili",
-    "Galician",
-    "Marathi",
-    "Punjabi",
-    "Sinhala",
-    "Khmer",
-    "Shona",
-    "Yoruba",
-    "Somali",
-    "Afrikaans",
-    "Occitan",
-    "Georgian",
-    "Belarusian",
-    "Tajik",
-    "Sindhi",
-    "Gujarati",
-    "Amharic",
-    "Yiddish",
-    "Lao",
-    "Uzbek",
-    "Faroese",
-    "Haitian Creole",
-    "Pashto",
-    "Turkmen",
-    "Nynorsk",
-    "Maltese",
-    "Sanskrit",
-    "Luxembourgish",
-    "Myanmar",
-    "Tibetan",
-    "Tagalog",
-    "Malagasy",
-    "Assamese",
-    "Tatar",
-    "Hawaiian",
-    "Lingala",
-    "Hausa",
-    "Bashkir",
-    "Javanese",
-    "Sundanese"
+LANGUAGES = [ 
+ "English", "Chinese", "German", "Spanish", "Russian", "Korean", 
+ "French", "Japanese", "Portuguese", "Turkish", "Polish", "Catalan", 
+ "Dutch", "Arabic", "Swedish", "Italian", "Indonesian", "Hindi", 
+ "Finnish", "Vietnamese", "Hebrew", "Ukrainian", "Greek", "Malay", 
+ "Czech", "Romanian", "Danish", "Hungarian", "Tamil", "Norwegian", 
+ "Thai", "Urdu", "Croatian", "Bulgarian", "Lithuanian", "Latin", 
+ "Maori", "Malayalam", "Welsh", "Slovak", "Telugu", "Persian", 
+ "Latvian", "Bengali", "Serbian", "Azerbaijani", "Slovenian", 
+ "Kannada", "Estonian", "Macedonian", "Breton", "Basque", "Icelandic", 
+ "Armenian", "Nepali", "Mongolian", "Bosnian", "Kazakh", "Albanian",
+ "Swahili", "Galician", "Marathi", "Punjabi", "Sinhala", "Khmer", 
+ "Shona", "Yoruba", "Somali", "Afrikaans", "Occitan", "Georgian", 
+ "Belarusian", "Tajik", "Sindhi", "Gujarati", "Amharic", "Yiddish", 
+ "Lao", "Uzbek", "Faroese", "Haitian Creole", "Pashto", "Turkmen", 
+ "Nynorsk", "Maltese", "Sanskrit", "Luxembourgish", "Myanmar", "Tibetan",
+ "Tagalog", "Malagasy", "Assamese", "Tatar", "Hawaiian", "Lingala", 
+ "Hausa", "Bashkir", "Javanese", "Sundanese"
 ]
 
 model_cache = dict()
 
-def greet(modelName, languageName, uploadFile, microphoneData, task):
+def transcribeFile(modelName, languageName, uploadFile, microphoneData, task):
     source = uploadFile if uploadFile is not None else microphoneData
     selectedLanguage = languageName.lower() if len(languageName) > 0 else None
     selectedModel = modelName if modelName is not None else "base"
@@ -142,6 +60,7 @@ def greet(modelName, languageName, uploadFile, microphoneData, task):
 
     return result["text"], segmentStream.read()
 
+
 ui_description = "Whisper is a general-purpose speech recognition model. It is trained on a large dataset of diverse " 
 ui_description += " audio and is also a multi-task model that can perform multilingual speech recognition "
 ui_description += " as well as speech translation and language identification. "
@@ -149,7 +68,7 @@ ui_description += " as well as speech translation and language identification. "
 if INPUT_AUDIO_MAX_DURATION > 0:
     ui_description += "\n\n" + "Max audio file length: " + str(INPUT_AUDIO_MAX_DURATION) + " s"
 
-demo = gr.Interface(fn=greet, description=ui_description, inputs=[
+demo = gr.Interface(fn=transcribeFile, description=ui_description, inputs=[
     gr.Dropdown(choices=["tiny", "base", "small", "medium", "large"], value="medium", label="Model"),
     gr.Dropdown(choices=sorted(LANGUAGES), label="Language"),
     gr.Audio(source="upload", type="filepath", label="Upload Audio"), 
