15219b49ff320e01ac7387e508065d44891e2f50
Kristian Stangeland <kr_stang@hotmail.com>
2022-09-27 01:11:22 +0200

    Fix SRT generation

--- utils.py
+++ utils.py
@@ -30,7 +30,7 @@ def compression_ratio(text) -> float:
     return len(text) / len(zlib.compress(text.encode("utf-8")))
 
 
-def format_timestamp(seconds: float, always_include_hours: bool = False):
+def format_timestamp(seconds: float, always_include_hours: bool = False, fractionalSeperator: str = '.'):
     assert seconds >= 0, "non-negative timestamp expected"
     milliseconds = round(seconds * 1000.0)
 
@@ -43,8 +43,8 @@ def format_timestamp(seconds: float, always_include_hours: bool = False):
     seconds = milliseconds // 1_000
     milliseconds -= seconds * 1_000
 
-    hours_marker = f"{hours}:" if always_include_hours or hours > 0 else ""
-    return f"{hours_marker}{minutes:02d}:{seconds:02d}.{milliseconds:03d}"
+    hours_marker = f"{hours:02d}:" if always_include_hours or hours > 0 else ""
+    return f"{hours_marker}{minutes:02d}:{seconds:02d}{fractionalSeperator}{milliseconds:03d}"
 
 
 def write_txt(transcript: Iterator[dict], file: TextIO):
@@ -79,8 +79,8 @@ def write_srt(transcript: Iterator[dict], file: TextIO):
         # write srt lines
         print(
             f"{i}\n"
-            f"{format_timestamp(segment['start'], always_include_hours=True)} --> "
-            f"{format_timestamp(segment['end'], always_include_hours=True)}\n"
+            f"{format_timestamp(segment['start'], always_include_hours=True, fractionalSeperator=',')} --> "
+            f"{format_timestamp(segment['end'], always_include_hours=True, fractionalSeperator=',')}\n"
             f"{segment['text'].strip().replace('-->', '->')}\n",
             file=file,
             flush=True,
