74b7d77c8ffadc14461b80269947f41d53e6408e
Kristian Stangeland <kr_stang@hotmail.com>
2023-05-01 18:59:36 +0200

    Adding JSON initial prompt
    By selecting "json_prompt_mode", you can
    customize the prompt to each segment.
    
    For instance:
                    [
                        {"segment_index": 0, "prompt": "Hello, how are you?"},
                        {"segment_index": 1, "prompt": "I'm doing well, how are you?"},
                        {"segment_index": 2, "prompt": "{0} Fine, thank you.", "format_prompt": true}
                    ]

--- src/whisper/abstractWhisperContainer.py
+++ src/whisper/abstractWhisperContainer.py
@@ -1,11 +1,16 @@
 import abc
 from typing import List
+
 from src.config import ModelConfig, VadInitialPromptMode
 
 from src.hooks.progressListener import ProgressListener
 from src.modelCache import GLOBAL_MODEL_CACHE, ModelCache
+from src.prompts.abstractPromptStrategy import AbstractPromptStrategy
 
 class AbstractWhisperCallback:
+    def __init__(self):
+        self.__prompt_mode_gpt = None
+
     @abc.abstractmethod
     def invoke(self, audio, segment_index: int, prompt: str, detected_language: str, progress_listener: ProgressListener = None):
         """
@@ -23,23 +28,6 @@ class AbstractWhisperCallback:
             A callback to receive progress updates.
         """
         raise NotImplementedError()
-
-    def _get_initial_prompt(self, initial_prompt: str, initial_prompt_mode: VadInitialPromptMode, 
-                               prompt: str, segment_index: int):
-        if (initial_prompt_mode == VadInitialPromptMode.PREPEND_ALL_SEGMENTS):
-            return self._concat_prompt(initial_prompt, prompt)
-        elif (initial_prompt_mode == VadInitialPromptMode.PREPREND_FIRST_SEGMENT):
-            return self._concat_prompt(initial_prompt, prompt) if segment_index == 0 else prompt
-        else:
-            raise ValueError(f"Unknown initial prompt mode {initial_prompt_mode}")
-
-    def _concat_prompt(self, prompt1, prompt2):
-        if (prompt1 is None):
-            return prompt2
-        elif (prompt2 is None):
-            return prompt1
-        else:
-            return prompt1 + " " + prompt2
 
 class AbstractWhisperContainer:
     def __init__(self, model_name: str, device: str = None, compute_type: str = "float16",
@@ -75,8 +63,8 @@ class AbstractWhisperContainer:
         pass
 
     @abc.abstractmethod
-    def create_callback(self, language: str = None, task: str = None, initial_prompt: str = None, 
-                        initial_prompt_mode: VadInitialPromptMode = VadInitialPromptMode.PREPREND_FIRST_SEGMENT, 
+    def create_callback(self, language: str = None, task: str = None, 
+                        prompt_strategy: AbstractPromptStrategy = None, 
                         **decodeOptions: dict) -> AbstractWhisperCallback:
         """
         Create a WhisperCallback object that can be used to transcript audio files.
@@ -87,11 +75,8 @@ class AbstractWhisperContainer:
             The target language of the transcription. If not specified, the language will be inferred from the audio content.
         task: str
             The task - either translate or transcribe.
-        initial_prompt: str
-            The initial prompt to use for the transcription.
-        initial_prompt_mode: VadInitialPromptMode
-            The mode to use for the initial prompt. If set to PREPEND_FIRST_SEGMENT, the initial prompt will be prepended to the first segment of audio.
-            If set to PREPEND_ALL_SEGMENTS, the initial prompt will be prepended to all segments of audio.
+        prompt_strategy: AbstractPromptStrategy
+            The prompt strategy to use for the transcription.
         decodeOptions: dict
             Additional options to pass to the decoder. Must be pickleable.
 
